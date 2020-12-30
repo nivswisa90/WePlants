2c7bd4da8642604eca277c45a1c22331f8b86e08
martinmazas <mazasmartin@gmail.com>
2020-12-30 18:26:40 +0200

    myFavorite server side done

--- Project/service/controllers/user.ctrl.js
+++ Project/service/controllers/user.ctrl.js
@@ -1,5 +1,6 @@
 const { query } = require('express');
 const User = require('../models/user');
+const Plant = require('../models/plants');
 
 
 let userID = 7;//to check how to make it dynamic!!!!!!
@@ -21,54 +22,58 @@ exports.userDBController = {
                 .catch(err => console.log(`Error getting the data from DB: ${err}`));
         }
 
-        else{
+        else {
             User.find({})
                 .then(docs => { res.json(docs) })
                 .catch(err => console.log(`Error getting the data from DB: ${err}`));
-    }
-},
+        }
+    },
 
     getUser(req, res) {
-    User.findOne({ id: parseInt(req.params.id) })
-        .then(docs => { res.json(docs) })
-        .catch(err => console.log(`Error getting the data from DB: ${err}`));
+        User.findOne({ id: parseInt(req.params.id) })
+            .then(docs => { res.json(docs) })
+            .catch(err => console.log(`Error getting the data from DB: ${err}`));
 
-},
+    },
 
-addUser(req, res) {
-    //Need to 
-    ++userID;
-    const newUser = new User({
-        "id": userID,
-        "first_name": req.body.first_name,
-        "last_name": req.body.last_name,
-        "email": req.body.email,
-        // "myFavorites": req.body.myFavorites
-    });
-    
-                                         
-    newUser.save()
-        .then(docs => { res.json(docs) })
-        .catch(err => console.log(`Error getting the data from DB: ${err}`));
-
-},
-
-// updateUser(req, res) {
-//     const toUpdate = req.body.way_of_care;
-//     [a,...rest] = [toUpdate];
-//     console.log(a);
-//     console.log(toUpdate);
-//     User.updateOne({ id: parseInt(req.params.id) },{$set: {...req.body}})
-//         .then(docs => { res.json(docs) })
-//         .catch(err => console.log(`Error getting the data from DB: ${err}`));
-// }
+    addUser(req, res) {
+        ++userID;
+        const newUser = new User({
+            "id": userID,
+            "first_name": req.body.first_name,
+            "last_name": req.body.last_name,
+            "email": req.body.email,
+            // "myFavorites": req.body.myFavorites
+        });
 
 
+        newUser.save()
+            .then(docs => { res.json(docs) })
+            .catch(err => console.log(`Error getting the data from DB: ${err}`));
 
-deleteUser(req, res) {
-    User.findOneAndDelete({ id: parseInt(req.params.id) })
-        .then(docs => { res.json(docs) })
-        .catch(err => console.log(`Error getting the data from DB: ${err}`));
-}
+    },
+    addMyFavorites(req, res) {
+        const name = req.query.name;
+        Plant.findOne({name: name})
+            .then((docs) => {
+
+                const id = 10;
+                const plant_name = docs.name;
+                const description = docs.description;
+                User.updateOne({ id: parseInt(req.params.id)} , {$push: {"myFavorites": {id: id, plant_name: plant_name, description: description}}})
+                    .then(docs => { res.json(docs) })
+                    .catch(err => console.log(`Error getting the data from DB: ${err}`));
+            })
+            .catch((err) => console.log(`Error getting the data from DB: ${err}`));
+
+        // User.updateOne({ id: parseInt(req.params.id) },{$set: {...req.body}})
+        //     .then(docs => { res.json(docs) })
+        //     .catch(err => console.log(`Error getting the data from DB: ${err}`));
+    },
+    deleteUser(req, res) {
+        User.findOneAndDelete({ id: parseInt(req.params.id) })
+            .then(docs => { res.json(docs) })
+            .catch(err => console.log(`Error getting the data from DB: ${err}`));
+    }
 };
 
--- Project/service/routers/userRouter.js
+++ Project/service/routers/userRouter.js
@@ -6,6 +6,7 @@ const userRouter = new Router();
 userRouter.get('/', userDBController.getUsers);
 userRouter.get('/:id', userDBController.getUser)
 userRouter.post('/', userDBController.addUser);
+userRouter.post('/:id', userDBController.addMyFavorites);       //We use post because we add new Object Plant to myFavorites
 // userRouter.put('/:id', userDBController.updateUser);
 userRouter.delete('/:id', userDBController.deleteUser);
 
