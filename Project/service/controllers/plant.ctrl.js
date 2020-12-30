bf3bc4b5c94769dd7af3e730c82699d33c344007
martinmazas <mazasmartin@gmail.com>
2020-12-30 10:11:48 +0200

    better use of find on db

--- Project/service/controllers/plant.ctrl.js
+++ Project/service/controllers/plant.ctrl.js
@@ -7,23 +7,27 @@ let plantID = 7;//to check how to make it dynamic!!!!!!
 exports.plantDBController = {
 
     getPlants(req, res) {
-        if (req.query.family) {
-            Plant.find({ family: `${req.query.family}` })
-                .then(docs => { res.json(docs) })
-                .catch(err => console.log(`Error getting the data from DB: ${err}`));
-        }
+        Plant.find({$or:[{name: req.query.name}, {family: req.query.name}]})
+        .then(docs => {res.json(docs)})
+        .catch(err => console.log(`Error getting the data from DB: ${err}`));
+            // res.status(404).send("Error"));
+        // if (req.query.family) {
+        //     Plant.find({ family: `${req.query.family}` })
+        //         .then(docs => { res.json(docs) })
+        //         .catch(err => console.log(`Error getting the data from DB: ${err}`));
+        // }
 
-        else if (req.query.name) {
-            Plant.find({ name: `${req.query.name}` })
-                .then(docs => {res.json(docs)})
-                .catch(err => console.log(`Error getting the data from DB: ${err}`));
-        }
+        // else if (req.query.name) {
+        //     Plant.find({ name: `${req.query.name}` })
+        //         .then(docs => {res.json(docs)})
+        //         .catch(err => console.log(`Error getting the data from DB: ${err}`));
+        // }
 
-        else{
-            Plant.find({})
-                .then(docs => { res.json(docs) })
-                .catch(err => console.log(`Error getting the data from DB: ${err}`));   
-        }
+        // else{
+        //     Plant.find({})
+        //         .then(docs => { res.json(docs) })
+        //         .catch(err => console.log(`Error getting the data from DB: ${err}`));   
+        // }
     },
 
     getPlant(req, res) {
--- Project/service/server.js
+++ Project/service/server.js
@@ -32,7 +32,7 @@ app.use('/api/users', userRouter);
 app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).send('Something is broken!');
-   });
+});
 
 
 app.listen(port, () => console.log('Express server is running on Port - ', port));
