099e937605a4f22f64d8d49efb54b24e1e1bfd3a
martinmazas <mazasmartin@gmail.com>
2020-12-31 11:51:53 +0200

    add validation to add in my favorites

--- Project/service/controllers/user.ctrl.js
+++ Project/service/controllers/user.ctrl.js
@@ -1,11 +1,7 @@
 const { query } = require('express');
 const User = require('../models/user');
 const Plant = require('../models/plants');
-
- 
-
-    
-
+const e = require('express');
 
 
 let userID = 7;
@@ -57,21 +53,59 @@ exports.userDBController = {
             .catch(err => console.log(`Error getting the data from DB: ${err}`));
 
     },
-    addMyFavorites(req, res) {
-        //Need to check if name exist in favorite
-        const name = req.query.name;
-        Plant.findOne({name: name})
-            .then((docs) => {
-
-                const id = 10;
-                const plant_name = docs.name;
-                const description = docs.description;
-                const image_url = docs.image_url;
-                User.updateOne({ id: parseInt(req.params.id)} , {$push: {"myFavorites": {id: id, plant_name: plant_name, description: description, image_url: image_url}}})
-                    .then(docs => { res.json(docs) })
-                    .catch(err => console.log(`Error getting the data from DB: ${err}`));
-            })
-            .catch((err) => console.log(`Error getting the data from DB: ${err}`));
+    updateUser(req, res) {
+        User.find({id: parseInt(req.params.id)})
+        .then(docs => {
+            const Favorites = docs[0].myFavorites;
+            const favLen = Favorites.length;
+            const plantName = req.query.name;
+            for(var i = 0; i<favLen; i++) {
+                if(plantName == Favorites[i].plant_name) {
+                    res.send('This plant is already added in your favorites');
+                    break;
+                }
+            }
+            if(i == favLen){
+                Plant.findOne({name: plantName})
+                .then((docs) => {
+                    if(docs != null){
+                        const id = 10;
+                        const plant_name = docs.name;
+                        const description = docs.description;
+                        const image_url = docs.image_url;
+                        User.updateOne({ id: parseInt(req.params.id)} , {$push: {"myFavorites": {id: id, plant_name: plant_name, description: description, image_url: image_url}}})
+                        .then(docs => { res.json(docs) })
+                        .catch(err => console.log(`Error getting the data from DB: ${err}`));
+                    }
+                    else{
+                        res.send('Plant not found');
+                    }
+                    // const id = 10;
+                    // const plant_name = docs.name;
+                    // const description = docs.description;
+                    // const image_url = docs.image_url;
+                    
+                    // User.updateOne({ id: parseInt(req.params.id)} , {$push: {"myFavorites": {id: id, plant_name: plant_name, description: description, image_url: image_url}}})
+                    //     .then(docs => { res.json(docs) })
+                    //     .catch(err => console.log(`Error getting the data from DB: ${err}`));
+                })
+                .catch((err) => console.log(`Error getting the data from DB: ${err}`));
+            }
+        })
+        .catch(err => console.log(`Error: ${err}`));
+        
+        // const name = req.query.name;
+        // Plant.findOne({name: name})
+        //     .then((docs) => {
+        //         const id = 10;
+        //         const plant_name = docs.name;
+        //         const description = docs.description;
+        //         const image_url = docs.image_url;
+        //         User.updateOne({ id: parseInt(req.params.id)} , {$push: {"myFavorites": {id: id, plant_name: plant_name, description: description, image_url: image_url}}})
+        //             .then(docs => { res.json(docs) })
+        //             .catch(err => console.log(`Error getting the data from DB: ${err}`));
+        //     })
+        //     .catch((err) => console.log(`Error getting the data from DB: ${err}`));
 
         // User.updateOne({ id: parseInt(req.params.id) },{$set: {...req.body}})
         //     .then(docs => { res.json(docs) })
