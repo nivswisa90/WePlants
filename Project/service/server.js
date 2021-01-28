2173602edde8da513343df83027f02f12aa3ba36
martinmazas <mazasmartin@gmail.com>
2021-01-28 19:52:57 +0200

    test cors

--- Project/service/server.js
+++ Project/service/server.js
@@ -14,9 +14,6 @@ const {weatherRouter} = require("./routers/weatherRouter");
 app.use(express.json());
 app.use(express.urlencoded({extended: true})); 
 
-
-
-
 app.use((req,res,next) =>{
     res.header('Access-Control-Allow-Headers','*');
     res.header('Access-Control-Allow-Methods', '*');
