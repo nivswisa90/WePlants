4bbd7dc88556fb50eca0e217ed3bc1bad6c08f84
martinmazas <mazasmartin@gmail.com>
2021-02-22 16:33:49 +0200

    working on flow 3

--- Project/service/controllers/mail.ctrl.js
+++ Project/service/controllers/mail.ctrl.js
@@ -21,29 +21,31 @@ exports.mailDBController = {
              Thanks!`
         };
 
-        // const wateringMail = {
-        //     from: process.env.GMAIL_USER,
-        //     to: req.body.email,
-        //     subject: 'Water advice',
-        //     text: `Hey dude! 
-        //     Time to watering plants! The plant ${req.body.plantName} is thirsty, here is our recommendation for watering ${req.body.plantName}:
-        //     ${req.body.wayOfCare.water}.
-        //     Have a good day, ${req.body.firstName}!`
-        // }
-
-        const mailOptions = (option) => {
-            transporter.sendMail(option, function (error, info) {
-                if (error) {
-                    console.log(error);
-                } else {
-                    console.log('Email sent: ' + info.response);
-                }
-            });
+        const wateringMail = {
+            from: process.env.GMAIL_USER,
+            to: req.body.email,
+            subject: 'Water advice',
+            text: 
+            `Hey ${req.body.firstName}! 
+            Time to watering plants! The plant ${req.body.plantName} is thirsty, here is our recommendation for watering ${req.body.plantName}:
+            ${req.body.water}!`
         }
+
+        // const mailOptions = (option) => {
+        //     transporter.sendMail(option, function (error, info) {
+        //         if (error) {
+        //             console.log(error);
+        //         } else {
+        //             console.log('Email sent: ' + info.response);
+        //         }
+        //     });
+        // }
         switch (req.body.type) {
             case 'suggestion':
                 mailOptions(suggestionMail);
                 break;
+            case 'notification':
+                mailOptions(wateringMail);
             default:
                 break;
         }
